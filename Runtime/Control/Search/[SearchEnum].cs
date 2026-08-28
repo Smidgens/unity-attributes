982d24e67f4bf2b79eac2389ba68a6d91aecceb1
Unnar <unnarvc@speedymail.org>
2026-08-28 11:07:39 +0000

    Overhaul SearchEnum to use advanced dropdown

--- Runtime/Control/Search/[SearchEnum].cs
+++ Runtime/Control/Search/[SearchEnum].cs
@@ -13,8 +13,10 @@ namespace Smidgenomics.Unity.Attributes
 namespace Smidgenomics.Unity.Attributes.Editor
 {
 	using System;
+	using System.Collections.Generic;
 	using UnityEditor;
 	using UnityEngine;
+	using UnityEditor.IMGUI.Controls;
 
 	[CustomPropertyDrawer(typeof(SearchEnumAttribute))]
 	internal sealed class _SearchEnumAttribute : __ControlDrawer<SearchEnumAttribute>
@@ -23,79 +25,112 @@ namespace Smidgenomics.Unity.Attributes.Editor
 
 		protected override void OnInit()
 		{
-			_enumValues = (int[])Enum.GetValues(_FieldType);
+			var vals = new List<(string, int)>();
+			var names = Enum.GetNames(_FieldType);
+			var values = (int[])Enum.GetValues(_FieldType);
+			for (int i = 0; i < names.Length; i++)
+			{
+				vals.Add((ObjectNames.NicifyVariableName(names[i]), values[i]));
+			}
+			_values = vals;
 		}
-
-		private string[] _enumNames;
-		private int[] _enumValues;
 
 		protected override void OnField(in DrawContext ctx)
 		{
 			var prop = ctx.property;
 
-			if (_enumNames == null)
+			var label = Enum.GetName(_FieldType, prop.intValue);
+
+			if (!string.IsNullOrEmpty(label))
 			{
-				_enumNames = prop.enumDisplayNames;
+				label = ObjectNames.NicifyVariableName(label);
 			}
-
-			_popupLabel.text = PluginConstants.Label.POPUP_UNSET;
-			
-			var ci = Array.IndexOf(_enumValues, prop.intValue);
-
-			if (ci > -1)
-			{
-				_popupLabel.text = _enumNames[ci];
-			}
-			
-			var enumType = _FieldType;
-
+			_popupLabel.text = label ?? PluginConstants.Label.POPUP_UNSET;
 			if (EditorGUI.DropdownButton(ctx.position, _popupLabel, FocusType.Keyboard))
 			{
-				if (_cachedTree == null)
+				var title = ObjectNames.NicifyVariableName(_FieldType.Name);
+				var enumDrop = new EnumDropdown(title, _values, prop.intValue, new AdvancedDropdownState())
 				{
-					_cachedTree = SearchPopup<int>.CreateTypeMenuTree(_FieldType.Name.ToSentenceCase(), new SearchPopup<int>.Options
+					onSelectValue = v =>
 					{
-						labelFn = v =>
-						{
-							var i = Array.IndexOf(_enumValues, v);
-
-							return i >= 0 ? _enumNames[i] : "-";
-						},
-						filterNameFn = v =>
-						{
-							var i = Array.IndexOf(_enumValues, v);
-
-							return i >= 0 ? _enumNames[i].ToLower() : v.ToString();
-						},
-						equalsFn = (a,b) => a == b
-					});
-
-					for (int i = 0; i < _enumNames.Length; i++)
-					{
-						var n = _enumNames[i];
-						var v = _enumValues[i];
-						_cachedTree.AddValue(n, v);
+						prop.intValue = v;
+						prop.serializedObject.ApplyModifiedProperties();
 					}
-				}
-				
-				// _cachedTree.BuildIndex();
-
-				_cachedTree.Filter(SearchPopup<int>.SearchFilter.Empty);
-
-				var menu = SearchPopup<int>.Create(0, _cachedTree, v =>
-				{
-					prop.intValue = v;
-					prop.serializedObject.ApplyModifiedProperties();
-				});
-				
-				menu.Show(ctx.position);
-				
-				
+				};
+				enumDrop.Show(ctx.position);
+				SetLastDropdownHeight(ctx.position, 300f);
 			}
 		}
 
+		private IReadOnlyList<(string, int)> _values;
 		private readonly GUIContent _popupLabel = new();
-		private SearchPopup<int>.MenuNode _cachedTree;
+
+		private sealed class EnumDropdown : AdvancedDropdown
+		{
+			private sealed class EnumDropdownItem : AdvancedDropdownItem
+			{
+				public int value { get; }
+				public EnumDropdownItem(string name, int value) : base(name)
+				{
+					this.value = value;
+				}
+			}
+
+			public Action<int> onSelectValue;
+
+			public EnumDropdown(string title, IReadOnlyList<(string, int)> values, int active, AdvancedDropdownState state) : base(state)
+			{
+				_values = values;
+				_active = active;
+				_title = title;
+			}
+
+			protected override void ItemSelected(AdvancedDropdownItem item)
+			{
+				onSelectValue?.Invoke(((EnumDropdownItem)(item)).value);
+			}
+
+			protected override AdvancedDropdownItem BuildRoot()
+			{
+				var root = new AdvancedDropdownItem(_title);
+
+				foreach (var (label, value) in _values)
+				{
+					var item = new EnumDropdownItem(label, value)
+					{
+						enabled = _active != value,
+						id = value
+					};
+					root.AddChild(item);
+				}
+				return root;
+			}
+
+			private readonly IReadOnlyList<(string, int)> _values;
+			private readonly int _active;
+			private readonly string _title;
+		}
+
+		// hack to control size of advanced dropdown window
+		private static void SetLastDropdownHeight(Rect rect, float maxHeight)
+		{
+			var window = EditorWindow.focusedWindow;
+
+			if(!window || window.GetType().Name != "AdvancedDropdownWindow")
+			{
+				return;
+			}
+			var position = window.position;
+			if(position.height <= maxHeight)
+			{
+				return;
+			}
+			position.height = maxHeight;
+			window.minSize = position.size;
+			window.maxSize = position.size;
+			window.position = position;
+			window.ShowAsDropDown(GUIUtility.GUIToScreenRect(rect), position.size);
+		}
 	}
 }
 
